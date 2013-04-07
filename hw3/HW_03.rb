6a6c673b29bf2a1a28f2bbadf1a11a21c538367b
Charlie <itcharlie@gmail.com>
2013-04-06 22:19:30 -0400

    HW 3 not solved

---
+++ hw3/HW_03.rb
@@ -0,0 +1,53 @@
+###############################################################################
+#
+# Introduction to Ruby on Rails
+#
+# Homework 03
+#
+# Purpose:
+#
+# Read the links below and complete the exercises in this file. This Lab
+# is to help you better understand Arrays, Hashes and Loops that we
+# learned in Lesson 03.
+#
+###############################################################################
+#
+# 1. Review your solution to Lab 02. Copy and Paste your solution to
+#    this file.
+#
+# 2. Create a new Array variable called `set_of_numbers`, this will be
+#    a range between 1 - 10 that our Player will guess from. Each value
+#    in the Array should be an integer.
+#
+# 3. Change the variable `secret_number` (HW_02) so that instead of a hard-coded
+#    Integer, it randomly chooses one of the options from `set_of_numbers`
+#    ('secret_number' is the integer our Player will try to guess).
+#
+#      Hint: Look up Array#sample in the Ruby documentation.
+#
+# 4. Create a new Hash variable called `messages`. In this Hash will be
+#    four Keys Value pairs:
+#
+#    1. :win - A String telling the Player that they have won.
+#
+#    2. :lose - A String telling the Player that they have lost and what
+#       the correct number was.
+#
+#    3. :too_low - A String telling the Player that their guess was too
+#       low.
+#
+#    4. :too_high - A String telling the Player that their guess was too
+#       high.
+#
+# 5. Change the behavior of your program, so instead of hard coding, use the principles of DRY(don't repeat yourself). 
+#    This means using a Loop to iterate over your code either until the Player has guessed the
+#    correct number, or they have tried to guess 3 times.
+#
+# 6. Make sure to comment your code so that you have appropriate
+#    documentation for you and for the TAs grading your homework. :-)
+#
+###############################################################################
+#
+#Put your solution below this line.
+#
+###############################################################################
